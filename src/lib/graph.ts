70bf7ab5592bb62b71a39e1c104d2620cb02b99f
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-01-25 17:56:11 +0100

    make setDimensions() more concise

--- src/lib/graph.ts
+++ src/lib/graph.ts
@@ -1,35 +1,26 @@
-import { Point } from './point';
 import { Polygon } from './polygon';
 
 type PolygonMap = { [color: string]: Polygon };
 
 export function setDimensions(scales: any, polygons: PolygonMap) {
-	const max = new Point();
-	const min = new Point(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
-	for (let [_, polygon] of Object.entries(polygons)) {
-		console.log(polygon);
-		const currMax = polygon.points.reduce((max, point) => {
-			max.x = Math.max(point.x, max.x);
-			max.y = Math.max(point.y, max.y);
-			return max;
-		}, new Point());
-
-		const currMin = polygon.points.reduce(
-			(min, point) => {
-				min.x = Math.min(point.x, min.x);
-				min.y = Math.min(point.y, min.y);
-				return min;
-			},
-			new Point(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+	const bounds = Object.values(polygons)
+		.flatMap((polygon) => polygon.points)
+		.reduce(
+			(acc, point) => ({
+				x: { max: Math.max(point.x, acc.x.max), min: Math.min(point.x, acc.x.min) },
+				y: { max: Math.max(point.y, acc.y.max), min: Math.min(point.y, acc.y.min) }
+			}),
+			{ x: { max: -Infinity, min: Infinity }, y: { max: -Infinity, min: Infinity } }
 		);
 
-		max.x = Math.max(currMax.x, max.x);
-		max.y = Math.max(currMax.y, max.y);
-		min.x = Math.min(currMin.x, min.x);
-		min.y = Math.min(currMin.y, min.y);
-	}
-	scales.x.max = max.x + 0.1 * (max.x - min.x);
-	scales.x.min = min.x - 0.1 * (max.x - min.x);
-	scales.y.max = max.y + 0.1 * (max.y - min.y);
-	scales.y.min = min.y - 0.1 * (max.y - min.y);
+	scales.x = {
+		...scales.x,
+		max: bounds.x.max + 0.1 * (bounds.x.max - bounds.x.min),
+		min: bounds.x.min - 0.1 * (bounds.x.max - bounds.x.min)
+	};
+	scales.y = {
+		...scales.y,
+		max: bounds.y.max + 0.1 * (bounds.y.max - bounds.y.min),
+		min: bounds.y.min - 0.1 * (bounds.y.max - bounds.y.min)
+	};
 }
