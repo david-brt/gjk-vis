a5945b4596aa08be0dc1828a585aa1b862a8bc65
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-01-25 17:35:40 +0100

    add automatic axis scaling

--- src/lib/Plot.svelte
+++ src/lib/Plot.svelte
@@ -38,7 +38,7 @@
 				x: {
 					max: 100,
 					min: -100,
-					position: 'center',
+					position: { y: 0 },
 					ticks: {
 						stepSize: 10
 					}
@@ -46,7 +46,7 @@
 				y: {
 					max: 100,
 					min: -100,
-					position: 'center',
+					position: { x: 0 },
 					ticks: {
 						stepSize: 10
 					}
@@ -79,8 +79,11 @@
 					chart.update('none');
 					return;
 				}
-				const mDiff = minkowskiDifference($polygons['red'], $polygons['blue']).getDrawable();
+				let mDiff = minkowskiDifference($polygons['red'], $polygons['blue']).getDrawable();
 				chart.data.datasets[2].data = mDiff;
+				mDiff = mDiff.map((point) => new Point(point.x, point.y));
+				$polygons['mDiff'] = new Polygon(mDiff as Point[]);
+				setDimensions(chart.options.scales, $polygons);
 				chart.update('none');
 			}
 		}
@@ -89,6 +92,39 @@
 		const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 		chart = new AutoChart(ctx, config);
 	});
+
+	type PolygonMap = { [color: string]: Polygon };
+
+	function setDimensions(scales: any, polygons: PolygonMap) {
+		const max = new Point();
+		const min = new Point(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+		for (let [_, polygon] of Object.entries(polygons)) {
+			console.log(polygon);
+			const currMax = polygon.points.reduce((max, point) => {
+				max.x = Math.max(point.x, max.x);
+				max.y = Math.max(point.y, max.y);
+				return max;
+			}, new Point());
+
+			const currMin = polygon.points.reduce(
+				(min, point) => {
+					min.x = Math.min(point.x, min.x);
+					min.y = Math.min(point.y, min.y);
+					return min;
+				},
+				new Point(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+			);
+
+			max.x = Math.max(currMax.x, max.x);
+			max.y = Math.max(currMax.y, max.y);
+			min.x = Math.min(currMin.x, min.x);
+			min.y = Math.min(currMin.y, min.y);
+		}
+		scales.x.max = max.x + 0.1 * (max.x - min.x);
+		scales.x.min = min.x - 0.1 * (max.x - min.x);
+		scales.y.max = max.y + 0.1 * (max.y - min.y);
+		scales.y.min = min.y - 0.1 * (max.y - min.y);
+	}
 </script>
 
 <div class="plot-container">
