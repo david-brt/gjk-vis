48fea5906ba860fddb1717aea608a89fc319aaf4
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-02-10 22:51:57 +0100

    update chart definition

--- src/lib/chartData.ts
+++ src/lib/chartData.ts
@@ -1,29 +1,28 @@
-import { polygons } from '$lib/store';
-import { get } from 'svelte/store';
-import { gjkState } from './store';
-
 export const data = {
-	datasets: generateData()
+	datasets: [
+		{
+			label: 'a',
+			data: [],
+			showLine: true
+		},
+		{
+			label: 'b',
+			data: [],
+			showLine: true
+		},
+		{
+			label: 'mDiff',
+			data: [],
+			showLine: true
+		},
+		{
+			label: 'simplex',
+			data: [],
+			showLine: true
+		},
+		{
+			label: 'v',
+			data: []
+		}
+	]
 } as Chart.ChartData;
-
-function generateData() {
-	const ps = get(polygons);
-	const res = [];
-	for (const key in ps) {
-		res.push({
-			label: key,
-			data: [],
-			showLine: true
-		});
-	}
-
-	const gjk = get(gjkState);
-	Object.keys(gjk).forEach((key) => {
-		res.push({
-			label: key,
-			data: [],
-			showLine: true
-		});
-	});
-	return res;
-}
